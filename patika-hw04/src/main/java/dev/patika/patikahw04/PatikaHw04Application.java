7322a18a98825839856091ccd3ba8399ea0abbcb
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-09-01 13:43:16 +0300

    Hw04 module created

---
+++ patika-hw04/src/main/java/dev/patika/patikahw04/PatikaHw04Application.java
@@ -0,0 +1,13 @@
+package dev.patika.patikahw04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikaHw04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatikaHw04Application.class, args);
+    }
+
+}
---
+++ patika-hw04/src/test/java/dev/patika/patikahw04/PatikaHw04ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.patikahw04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatikaHw04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
